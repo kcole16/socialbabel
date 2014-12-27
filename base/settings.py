67110526bbcef0782730f780f3ec11b156a0a465
Kendall Cole <kendall@veenome.com>
2014-12-27 13:31:44 -0500

    Try

--- base/settings.py
+++ base/settings.py
@@ -61,16 +61,16 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'babelbuffer',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': ''
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'babelbuffer',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 # Internationalization
